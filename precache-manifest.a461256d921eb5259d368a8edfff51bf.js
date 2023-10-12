34dbc099e0952a9c6df90a63b8a61c5bce17b6d4
Sandip Satyal <sandipsatyal25@gmail.com>
2023-10-12 10:13:24 +0545

    Updates

---
+++ precache-manifest.a461256d921eb5259d368a8edfff51bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041da1fdc90094edfee07db9e90eff0c",
+    "url": "/Image-Cropper/index.html"
+  },
+  {
+    "revision": "c49e3b9bc7b83d3ae1a0",
+    "url": "/Image-Cropper/static/css/main.4b863b34.chunk.css"
+  },
+  {
+    "revision": "a0ff814c9560ca8c857c",
+    "url": "/Image-Cropper/static/js/2.e512d673.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Image-Cropper/static/js/2.e512d673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c49e3b9bc7b83d3ae1a0",
+    "url": "/Image-Cropper/static/js/main.bd80e1b2.chunk.js"
+  },
+  {
+    "revision": "b3393756c389fe42131e",
+    "url": "/Image-Cropper/static/js/runtime-main.fbcc8d25.js"
+  }
+]);
